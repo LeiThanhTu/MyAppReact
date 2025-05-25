710f22c2b80aa8e29ade1bd4cd1e4b41e96d5f7c
LeiThanhTu <lethithanhtu18@gmail.com>
2025-05-25 22:06:24 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import Toggle from "./components/state/Toggle";
+import Game from "./components/tictactoe/Game";
 import YoutubeList from "./components/youtube/YoutubeList";
 
 function App() {
  const name = "ABCD"
   return (
   <div>
-     <YoutubeList>
+     {/* <YoutubeList>
       {name}
     </YoutubeList> 
 
-    <Toggle></Toggle>
+    <Toggle></Toggle> */}
+    <Game></Game>
+
   </div>
   );
 }
